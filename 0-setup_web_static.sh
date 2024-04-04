37d82bee14886b4eb15d5e964f203866e7961a07
Gurnelius <terergurnelius@gmail.com>
2024-04-04 14:27:46 +0300

    Initial commit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,10 +4,22 @@
 apt-get update
 apt-get install -y nginx
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+
+sudo echo "<html>
+  <head>
+  <title>Air BnB Clone</title>
+  </head>
+  <body>
+    <h2>Holberton School<h2>
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
